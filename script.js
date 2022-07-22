296e100db77b5deb9f2484603f15f49d682738c3
ismael lima <ismaellimadeolivera2004@gmail.com>
2022-07-22 17:39:56 -0300

    adicionado controle a snake e impedir que ela saia da tela

--- script.js
+++ script.js
@@ -20,7 +20,21 @@ function criarCobrinha(){
   }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+  if(event.keyCode == 65 && direction != "right") direction = "left";
+  if(event.keyCode == 87 && direction != "down") direction = "up";
+  if(event.keyCode == 68 && direction != "left") direction = "right";
+  if(event.keyCode == 83 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
+  if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+  if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;          
+
   criarBG();
   criarCobrinha();
 
